04b343131ee3b8c02de2f97bb76978d6edf018e2
Nipitare <nipitareyt@gmail.com>
2025-11-17 00:01:01 +0200

    Fence model

--- data.js
+++ data.js
@@ -77,16 +77,16 @@ const blockData = {
             const isSouth = states.generated.connection_south
 
             let returnCode = ''
-// TODO: Use fencses model instead of walls one
+
             returnCode = returnCode + `o Cube
-                v ${x+.75} ${y+1} ${z+.25}
-                v ${x+.75} ${y} ${z+.25}
-                v ${x+.75} ${y+1} ${z+.75}
-                v ${x+.75} ${y} ${z+.75}
-                v ${x+.25} ${y+1} ${z+.25}
-                v ${x+.25} ${y} ${z+.25}
-                v ${x+.25} ${y+1} ${z+.75}
-                v ${x+.25} ${y} ${z+.75}
+                v ${x+.625} ${y+1} ${z+.375}
+                v ${x+.625} ${y} ${z+.375}
+                v ${x+.625} ${y+1} ${z+.625}
+                v ${x+.625} ${y} ${z+.625}
+                v ${x+.375} ${y+1} ${z+.375}
+                v ${x+.375} ${y} ${z+.375}
+                v ${x+.375} ${y+1} ${z+.625}
+                v ${x+.375} ${y} ${z+.625}
 
                 f ${1+vert} ${5+vert} ${7+vert} ${3+vert}
                 f ${4+vert} ${3+vert} ${7+vert} ${8+vert}
@@ -98,79 +98,135 @@ const blockData = {
 
             if (isNorth) {
                 returnCode += `
-                    v ${x+.6875} ${y+.8125} ${z}
-                    v ${x+.6875} ${y} ${z}
-                    v ${x+.6875} ${y+.8125} ${z+.25}
-                    v ${x+.6875} ${y} ${z+.25}
-                    v ${x+.3125} ${y+.8125} ${z}
-                    v ${x+.3125} ${y} ${z}
-                    v ${x+.3125} ${y+.8125} ${z+.25}
-                    v ${x+.3125} ${y} ${z+.25}
+                    v ${x+.625} ${y+.9375} ${z}
+                    v ${x+.625} ${y+.75} ${z}
+                    v ${x+.625} ${y+.9375} ${z+.375}
+                    v ${x+.625} ${y+.75} ${z+.375}
+                    v ${x+.375} ${y+.9375} ${z}
+                    v ${x+.375} ${y+.75} ${z}
+                    v ${x+.375} ${y+.9375} ${z+.375}
+                    v ${x+.375} ${y+.75} ${z+.375}
+                    v ${x+.625} ${y+.5625} ${z}
+                    v ${x+.625} ${y+.375} ${z}
+                    v ${x+.625} ${y+.5625} ${z+.375}
+                    v ${x+.625} ${y+.375} ${z+.375}
+                    v ${x+.375} ${y+.5625} ${z}
+                    v ${x+.375} ${y+.375} ${z}
+                    v ${x+.375} ${y+.5625} ${z+.375}
+                    v ${x+.375} ${y+.375} ${z+.375}
 
                     f ${1+vert} ${5+vert} ${7+vert} ${3+vert}
                     f ${4+vert} ${3+vert} ${7+vert} ${8+vert}
                     f ${8+vert} ${7+vert} ${5+vert} ${6+vert}
                     f ${6+vert} ${2+vert} ${4+vert} ${8+vert}
                     f ${2+vert} ${1+vert} ${3+vert} ${4+vert}
-                    f ${6+vert} ${5+vert} ${1+vert} ${2+vert}\n`
-                vert += 8
+                    f ${6+vert} ${5+vert} ${1+vert} ${2+vert}
+                    f ${9+vert} ${13+vert} ${15+vert} ${11+vert}
+                    f ${12+vert} ${11+vert} ${15+vert} ${16+vert}
+                    f ${16+vert} ${15+vert} ${13+vert} ${14+vert}
+                    f ${14+vert} ${10+vert} ${12+vert} ${16+vert}
+                    f ${10+vert} ${9+vert} ${11+vert} ${12+vert}
+                    f ${14+vert} ${13+vert} ${9+vert} ${10+vert}\n`
+                vert += 16
             }
             if (isEast) {
                 returnCode += `
-                    v ${x+1} ${y+.8125} ${z+.3125}
-                    v ${x+1} ${y} ${z+.3125}
-                    v ${x+1} ${y+.8125} ${z+.6875}
-                    v ${x+1} ${y} ${z+.6875}
-                    v ${x+.75} ${y+.8125} ${z+.3125}
-                    v ${x+.75} ${y} ${z+.3125}
-                    v ${x+.75} ${y+.8125} ${z+.6875}
-                    v ${x+.75} ${y} ${z+.6875}
+                    v ${x+1} ${y+.9375} ${z+.375}
+                    v ${x+1} ${y+.75} ${z+.375}
+                    v ${x+1} ${y+.9375} ${z+.625}
+                    v ${x+1} ${y+.75} ${z+.625}
+                    v ${x+.625} ${y+.9375} ${z+.375}
+                    v ${x+.625} ${y+.75} ${z+.375}
+                    v ${x+.625} ${y+.9375} ${z+.625}
+                    v ${x+.625} ${y+.75} ${z+.625}
+                    v ${x+1} ${y+.5625} ${z+.375}
+                    v ${x+1} ${y+.375} ${z+.375}
+                    v ${x+1} ${y+.5625} ${z+.625}
+                    v ${x+1} ${y+.375} ${z+.625}
+                    v ${x+.625} ${y+.5625} ${z+.375}
+                    v ${x+.625} ${y+.375} ${z+.375}
+                    v ${x+.625} ${y+.5625} ${z+.625}
+                    v ${x+.625} ${y+.375} ${z+.625}
 
                     f ${1+vert} ${5+vert} ${7+vert} ${3+vert}
                     f ${4+vert} ${3+vert} ${7+vert} ${8+vert}
                     f ${8+vert} ${7+vert} ${5+vert} ${6+vert}
                     f ${6+vert} ${2+vert} ${4+vert} ${8+vert}
                     f ${2+vert} ${1+vert} ${3+vert} ${4+vert}
-                    f ${6+vert} ${5+vert} ${1+vert} ${2+vert}\n`
-                vert += 8
+                    f ${6+vert} ${5+vert} ${1+vert} ${2+vert}
+                    f ${9+vert} ${13+vert} ${15+vert} ${11+vert}
+                    f ${12+vert} ${11+vert} ${15+vert} ${16+vert}
+                    f ${16+vert} ${15+vert} ${13+vert} ${14+vert}
+                    f ${14+vert} ${10+vert} ${12+vert} ${16+vert}
+                    f ${10+vert} ${9+vert} ${11+vert} ${12+vert}
+                    f ${14+vert} ${13+vert} ${9+vert} ${10+vert}\n`
+                vert += 16
             }
             if (isSouth) {
                 returnCode += `
-                    v ${x+.6875} ${y+.8125} ${z+.75}
-                    v ${x+.6875} ${y} ${z+.75}
-                    v ${x+.6875} ${y+.8125} ${z+1}
-                    v ${x+.6875} ${y} ${z+1}
-                    v ${x+.3125} ${y+.8125} ${z+.75}
-                    v ${x+.3125} ${y} ${z+.75}
-                    v ${x+.3125} ${y+.8125} ${z+1}
-                    v ${x+.3125} ${y} ${z+1}
+                    v ${x+.625} ${y+.9375} ${z+.625}
+                    v ${x+.625} ${y+.75} ${z+.625}
+                    v ${x+.625} ${y+.9375} ${z+1}
+                    v ${x+.625} ${y+.75} ${z+1}
+                    v ${x+.375} ${y+.9375} ${z+.625}
+                    v ${x+.375} ${y+.75} ${z+.625}
+                    v ${x+.375} ${y+.9375} ${z+1}
+                    v ${x+.375} ${y+.75} ${z+1}
+                    v ${x+.625} ${y+.5625} ${z+.625}
+                    v ${x+.625} ${y+.375} ${z+.625}
+                    v ${x+.625} ${y+.5625} ${z+1}
+                    v ${x+.625} ${y+.375} ${z+1}
+                    v ${x+.375} ${y+.5625} ${z+.625}
+                    v ${x+.375} ${y+.375} ${z+.625}
+                    v ${x+.375} ${y+.5625} ${z+1}
+                    v ${x+.375} ${y+.375} ${z+1}
 
                     f ${1+vert} ${5+vert} ${7+vert} ${3+vert}
                     f ${4+vert} ${3+vert} ${7+vert} ${8+vert}
                     f ${8+vert} ${7+vert} ${5+vert} ${6+vert}
                     f ${6+vert} ${2+vert} ${4+vert} ${8+vert}
                     f ${2+vert} ${1+vert} ${3+vert} ${4+vert}
-                    f ${6+vert} ${5+vert} ${1+vert} ${2+vert}\n`
-                vert += 8
+                    f ${6+vert} ${5+vert} ${1+vert} ${2+vert}
+                    f ${9+vert} ${13+vert} ${15+vert} ${11+vert}
+                    f ${12+vert} ${11+vert} ${15+vert} ${16+vert}
+                    f ${16+vert} ${15+vert} ${13+vert} ${14+vert}
+                    f ${14+vert} ${10+vert} ${12+vert} ${16+vert}
+                    f ${10+vert} ${9+vert} ${11+vert} ${12+vert}
+                    f ${14+vert} ${13+vert} ${9+vert} ${10+vert}\n`
+                vert += 16
             }
             if (isWest) {
                 returnCode += `
-                    v ${x+.25} ${y+.8125} ${z+.3125}
-                    v ${x+.25} ${y} ${z+.3125}
-                    v ${x+.25} ${y+.8125} ${z+.6875}
-                    v ${x+.25} ${y} ${z+.6875}
-                    v ${x} ${y+.8125} ${z+.3125}
-                    v ${x} ${y} ${z+.3125}
-                    v ${x} ${y+.8125} ${z+.6875}
-                    v ${x} ${y} ${z+.6875}
+                    v ${x+.375} ${y+.9375} ${z+.375}
+                    v ${x+.375} ${y+.75} ${z+.375}
+                    v ${x+.375} ${y+.9375} ${z+.625}
+                    v ${x+.375} ${y+.75} ${z+.625}
+                    v ${x} ${y+.9375} ${z+.375}
+                    v ${x} ${y+.75} ${z+.375}
+                    v ${x} ${y+.9375} ${z+.625}
+                    v ${x} ${y+.75} ${z+.625}
+                    v ${x+.375} ${y+.5625} ${z+.375}
+                    v ${x+.375} ${y+.375} ${z+.375}
+                    v ${x+.375} ${y+.5625} ${z+.625}
+                    v ${x+.375} ${y+.375} ${z+.625}
+                    v ${x} ${y+.5625} ${z+.375}
+                    v ${x} ${y+.375} ${z+.375}
+                    v ${x} ${y+.5625} ${z+.625}
+                    v ${x} ${y+.375} ${z+.625}
 
                     f ${1+vert} ${5+vert} ${7+vert} ${3+vert}
                     f ${4+vert} ${3+vert} ${7+vert} ${8+vert}
                     f ${8+vert} ${7+vert} ${5+vert} ${6+vert}
                     f ${6+vert} ${2+vert} ${4+vert} ${8+vert}
                     f ${2+vert} ${1+vert} ${3+vert} ${4+vert}
-                    f ${6+vert} ${5+vert} ${1+vert} ${2+vert}\n`
-                vert += 8
+                    f ${6+vert} ${5+vert} ${1+vert} ${2+vert}
+                    f ${9+vert} ${13+vert} ${15+vert} ${11+vert}
+                    f ${12+vert} ${11+vert} ${15+vert} ${16+vert}
+                    f ${16+vert} ${15+vert} ${13+vert} ${14+vert}
+                    f ${14+vert} ${10+vert} ${12+vert} ${16+vert}
+                    f ${10+vert} ${9+vert} ${11+vert} ${12+vert}
+                    f ${14+vert} ${13+vert} ${9+vert} ${10+vert}\n`
+                vert += 16
             }
 
             return [returnCode, vert]
